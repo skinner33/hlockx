2c154becd6584385e6b83bd734b8f2a709bef540
Martin Perner <martin@perner.cc>
2012-01-05 16:06:07 +0100

    Create black window

--- src/hlockx.hs
+++ src/hlockx.hs
@@ -3,7 +3,11 @@ module Main (
               main
             ) where
 
+import Data.Bits ((.|.))
+
 import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras
+import Graphics.X11.Xlib.Types
 
 import System.Posix.User
 import System.Unix.Crypt
@@ -15,9 +19,33 @@ getPasswordHash = do
 	entry <- getSUserEntryForName username
 	return (sUserPassword entry)
 
+makeWin :: Display -> ScreenNumber -> Window -> IO Window
+makeWin dpy scrNr rw = do
+	let scr = screenOfDisplay dpy scrNr
+	    bp = blackPixel dpy scrNr
+	    visual   = defaultVisualOfScreen scr
+	    depth    = defaultDepthOfScreen scr
+	    attrmask = (cWOverrideRedirect .|.  cWBackPixel)
+	    w = fromIntegral $ displayWidth dpy scrNr
+	    h = fromIntegral $ displayHeight dpy scrNr
+	allocaSetWindowAttributes $
+		\attributes -> do
+			set_override_redirect attributes True
+			set_background_pixel attributes bp
+			createWindow dpy rw 0 0 w h 0 depth copyFromParent
+			             visual attrmask attributes
+
+
+
 main :: IO ()
 main = do
 	dpy <- openDisplay ""
+	let srcNr = defaultScreen dpy
+	root <- rootWindow dpy srcNr
+	win <- makeWin dpy srcNr root
+	mapRaised dpy win
+	ret <- grabPointer dpy win False (buttonPressMask .|. buttonReleaseMask .|. pointerMotionMask) grabModeAsync grabModeAsync none none currentTime
+
 	pw <- getPasswordHash
 	input <- getLine
 	encrypt <- crypt input pw
@@ -25,6 +53,8 @@ main = do
 		putStrLn "Yes"
 	 else
 		putStrLn "No"
+
+	destroyWindow dpy win
 	closeDisplay dpy
 
 
