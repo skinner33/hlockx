d37fccdd596e5915d8b2446b5ed6454ca7a6ab5c
Martin Perner <martin@perner.cc>
2012-01-05 19:10:29 +0100

    basic functionality

--- src/hlockx.hs
+++ src/hlockx.hs
@@ -4,10 +4,13 @@ module Main (
             ) where
 
 import Data.Bits ((.|.))
+import Data.Char (isControl)
 
 import Control.Monad (when)
 
+import Graphics.X11.Types
 import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Event
 import Graphics.X11.Xlib.Extras
 import Graphics.X11.Xlib.Types
 
@@ -19,6 +22,8 @@ import System.Exit
 import System.Posix.User
 import System.Unix.Crypt
 import System.Unix.Shadow
+
+import System.IO.Unsafe
 
 getPasswordHash :: IO String
 getPasswordHash = do
@@ -48,10 +53,71 @@ cleanup dpy win = do
 	destroyWindow dpy win
 	closeDisplay dpy
 
+processInput :: String -> String -> Maybe KeySym -> String -> Maybe String
+processInput pw input Nothing _ = Just input
+processInput pw input (Just ksym) str
+	| ksym `elem` [xK_Return, xK_KP_Enter, xK_Escape] = Just ""
+	| ksym == xK_BackSpace = Just $ safeInit input
+	| xK_KP_0 <= ksym || ksym <= xK_KP_9 = checkPasswords pw $ input ++ str
+	| isFunctionKey ksym || isKeypadKey ksym || isMiscFunctionKey ksym = Just input
+	| isPFKey ksym || isPrivateKeypadKey ksym = Just input
+	| safeNotControl str = checkPasswords pw $ input ++ str
+	| otherwise = Just input
+
+-- Takes a password and a string and checks if
+-- the password is equal to a substring of the string
+-- from the tail of the string starting
+checkPasswords :: String -> String -> Maybe String
+checkPasswords pw str = checkPasswords' pw (safeInit str) (safeLast str)
+
+-- Checks if the password can be build of the remaining string
+-- so that only the tail of the remaing string appended in the
+-- front of the string to check
+checkPasswords' :: String -> String -> String -> Maybe String
+checkPasswords' pw rem str
+	| checkPassword pw str = Nothing
+	| rem == "" = Just str
+	| otherwise = checkPasswords' pw (safeInit rem) (safeLast rem ++ str)
+
+checkPassword :: String -> String -> Bool
+checkPassword pw str
+	| encrypt == pw = True
+	| otherwise = False
+		where encrypt = unsafePerformIO $ crypt str pw
+
+safeNotControl :: String -> Bool
+safeNotControl "" = False
+safeNotControl (x:xs) = not $ isControl x
+
+safeInit :: String -> String
+safeInit "" = ""
+safeInit str = init str
+
+safeLast :: String -> String
+safeLast "" = ""
+safeLast str = [last str]
+
+eventLoop :: Display -> String -> IO ()
+eventLoop dpy pw =
+	allocaXEvent $ \e -> eventLoop' dpy pw e ""
+
+eventLoop' :: Display -> String -> XEventPtr -> String -> IO ()
+eventLoop' dpy pw e input = do
+	nextEvent dpy e
+	et <- get_EventType e
+	if et == keyPress then do
+		(ksym, str) <- lookupString $ asKeyEvent e
+		case processInput pw input ksym str of
+			Just a -> eventLoop' dpy pw e a
+			Nothing -> return ()
+	 else eventLoop' dpy pw e input
+
+
 
 main :: IO ()
 main = do
 	progName <- getProgName
+	pw <- getPasswordHash
 	dpy <- openDisplay ""
 	let srcNr = defaultScreen dpy
 	root <- rootWindow dpy srcNr
@@ -70,13 +136,8 @@ main = do
 		cleanup dpy win
 		exitFailure
 
-	pw <- getPasswordHash
-	input <- getLine
-	encrypt <- crypt input pw
-	if encrypt == pw then
-		putStrLn "Yes"
-	 else
-		putStrLn "No"
+	sync dpy True
+	eventLoop dpy pw
 
 	cleanup dpy win
 
